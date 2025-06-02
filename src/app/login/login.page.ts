c56531b0d8033ab32409ac09db3c74c63b9d2bc3
Adrian <eslenderdark@gmail.com>
2025-06-02 16:15:57 +0200

    asd

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,48 +1,43 @@
-import { Component, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '@auth0/auth0-angular';
-import { DOCUMENT } from '@angular/common';
-import { IonContent, IonToolbar, IonTitle, IonButton } from '@ionic/angular/standalone'
+import { IonHeader, IonToolbar, IonMenuButton, IonButton,IonAlert, IonButtons, IonTitle, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, IonImg } from '@ionic/angular/standalone';
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  imports: [IonContent, IonToolbar, IonTitle, IonButton]
-
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonMenuButton, IonButton,IonAlert, IonButtons, IonTitle, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, IonImg]
 })
-export class LoginPage {
-  constructor(private auth: AuthService, @Inject(DOCUMENT) public document: Document) { }
+export class LoginPage implements OnInit {
 
   public user: any;
-  public usuario: any;
+  constructor(public auth: AuthService) { }
 
-  async ngOnInit() {
-    this.auth.user$.subscribe((data) => {
+  ngOnInit() {
+    this.auth.user$.subscribe((data) =>{
       this.user = data;
       console.log(this.user);
-      this.usuario = {
-        name: this.user.given_name,
-        email: this.user.email,
-        foto: this.user.picture,
-      };
+  
+      if (this.user) {
+        let usuario = {
+          user: this.user.name,
+          email: this.user.email,
+          picture: this.user.picture
+        };
+        localStorage.setItem('user', JSON.stringify(usuario));
+      }
     });
   }
   
 
-  
   login() {
     this.auth.loginWithRedirect({
       appState: {
         target: '/home'
-      }
-    });
-  }
-
-  logout() {
-    this.auth.logout({
-      logoutParams: {
-        returnTo: this.document.location.origin
       }
     });
   }
